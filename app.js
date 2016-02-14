7af81a9e8b2f7ae275ce916b657b3d55211bcbe7
support <support@eatj.com>
2016-02-13 17:02:56 -0800

    Change

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	
-server.listen(8002);
+
+var port = 0; //Please put your port number here. You can find your port number at http://nodejs.eatj.com/account.jsp
+
+if(port == 0) {
+	console.log('Please find your port number at http://nodejs.eatj.com/account.jsp and change it in this file');
+	process.exit();
+}
+server.listen(port);
 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');
